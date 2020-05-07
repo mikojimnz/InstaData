5fba9a1fd00d509955ed566077854de0a3e444f6
mikojimnz <mikojimnz@users.noreply.github.com>
2020-05-07 13:16:21 -0400

    Complete upload UI.

--- js/upload.js
+++ js/upload.js
@@ -1,3 +1,26 @@
+/*eslint-env jquery*/
+
 $(window).on('load', function () {
+    'use strict';
+
     $('#uploadModal').modal('show');
+
+    $("#upload-submit").click(function (event) {
+        event.preventDefault();
+
+        if ($('#fileUploadConnections').val().split('\\').pop() === "connections.json") {
+            $('#fileUploadConnectionsWarning').text("");
+
+            var fileReader = new FileReader();
+            fileReader.onload = function () {
+                var connections = JSON.parse(fileReader.result);
+                console.log(connections);
+            };
+            fileReader.readAsText($('#fileUploadConnections').prop('files')[0]);
+
+            $('#uploadModal').modal('hide');
+        } else {
+            $('#fileUploadConnectionsWarning').text("Error: This file is not connections.json");
+        }
+    });
 });
