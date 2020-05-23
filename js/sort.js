f58f5d55280ce49fca79e0c50099617565ef9b4d
mikojimnz <mikojimnz@users.noreply.github.com>
2020-05-22 21:52:38 -0400

    Made unselected filters more visible

--- js/sort.js
+++ js/sort.js
@@ -8,6 +8,10 @@ $(window).on('load', function () {
         $("#followerCF").removeClass("btn-success");
         $("#followingFB").removeClass("btn-warning");
         $("#followingCF").removeClass("btn-success");
+        $("#followerFB").addClass("btn-light");
+        $("#followerCF").addClass("btn-light");
+        $("#followingFB").addClass("btn-light");
+        $("#followingCF").addClass("btn-light");
     }
 
     function updateStats() {
@@ -19,6 +23,7 @@ $(window).on('load', function () {
         if ($(this).hasClass("btn-primary")) {
             $('#tableConnectionsFollowers').bootstrapTable('filterBy', {});
             $('#tableConnectionsFollowing').bootstrapTable('filterBy', {});
+            $(this).addClass("btn-light");
             $(this).removeClass("btn-primary");
             clearFilters();
         } else {
@@ -28,6 +33,7 @@ $(window).on('load', function () {
             $('#tableConnectionsFollowing').bootstrapTable('filterBy', {
                 FB: 'Yes'
             });
+            $(this).removeClass("btn-light");
             $(this).addClass("btn-primary");
             clearFilters();
         }
@@ -38,6 +44,7 @@ $(window).on('load', function () {
     $("#followerCF").click(function () {
         if ($(this).hasClass("btn-success")) {
             $('#tableConnectionsFollowers').bootstrapTable('filterBy', {});
+            $(this).addClass("btn-light");
             $(this).removeClass("btn-success");
             $("#followerFB").removeClass("btn-warning");
             $("#followFFollow").removeClass("btn-primary");
@@ -45,6 +52,7 @@ $(window).on('load', function () {
             $('#tableConnectionsFollowers').bootstrapTable('filterBy', {
                 extra: 'Yes'
             });
+            $(this).removeClass("btn-light");
             $(this).addClass("btn-success");
             $("#followerFB").removeClass("btn-warning");
             $("#followFFollow").removeClass("btn-primary");
@@ -56,6 +64,7 @@ $(window).on('load', function () {
     $("#followerFB").click(function () {
         if ($(this).hasClass("btn-warning")) {
             $('#tableConnectionsFollowers').bootstrapTable('filterBy', {});
+            $(this).addClass("btn-light");
             $(this).removeClass("btn-warning");
             $("#followerCF").removeClass("btn-success");
             $("#followFFollow").removeClass("btn-primary");
@@ -63,6 +72,7 @@ $(window).on('load', function () {
             $('#tableConnectionsFollowers').bootstrapTable('filterBy', {
                 FB: 'No'
             });
+            $(this).removeClass("btn-light");
             $(this).addClass("btn-warning");
             $("#followerCF").removeClass("btn-success");
             $("#followFFollow").removeClass("btn-primary");
@@ -74,6 +84,7 @@ $(window).on('load', function () {
     $("#followingCF").click(function () {
         if ($(this).hasClass("btn-success")) {
             $('#tableConnectionsFollowing').bootstrapTable('filterBy', {});
+            $(this).addClass("btn-light");
             $(this).removeClass("btn-success");
             $("#followingFB").removeClass("btn-warning");
             $("#followFFollow").removeClass("btn-primary");
@@ -81,6 +92,7 @@ $(window).on('load', function () {
             $('#tableConnectionsFollowing').bootstrapTable('filterBy', {
                 extra: 'Yes'
             });
+            $(this).removeClass("btn-light");
             $(this).addClass("btn-success");
             $("#followingFB").removeClass("btn-warning");
             $("#followFFollow").removeClass("btn-primary");
@@ -92,6 +104,7 @@ $(window).on('load', function () {
     $("#followingFB").click(function () {
         if ($(this).hasClass("btn-warning")) {
             $('#tableConnectionsFollowing').bootstrapTable('filterBy', {});
+            $(this).addClass("btn-light");
             $(this).removeClass("btn-warning");
             $("#followingCF").removeClass("btn-success");
             $("#followFFollow").removeClass("btn-primary");
@@ -99,6 +112,7 @@ $(window).on('load', function () {
             $('#tableConnectionsFollowing').bootstrapTable('filterBy', {
                 FB: 'No'
             });
+            $(this).removeClass("btn-light");
             $(this).addClass("btn-warning");
             $("#followingCF").removeClass("btn-success");
             $("#followFFollow").removeClass("btn-primary");
